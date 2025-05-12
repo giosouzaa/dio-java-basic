787c96fed4745bcee87f7ce2f47c9770fa20c44d
giosouzaa <sgiovanna926@gmail.com>
2025-05-12 17:27:07 -0300

    operadores

---
+++ primeiro projeto java/operadores/src/operadores_unarios.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+    }
+}
---
+++ primeiro projeto java/tipos-e-variaveis/teste/src/App.java
@@ -0,0 +1,14 @@
+public class operadores_unarios {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+    }
+}
